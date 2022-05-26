980868bac661a231862824a8d67513faab588976
Faiqresulzade <faigbr@code.edu.az>
2022-05-26 18:21:47 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            { 
+                st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            }
+        }
+    }
+}
---
+++ ConsoleApp18/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Student
+    {
+        public int[] grades;
+        public int [] Grades
+        {
+            get
+            {
+                return grades;
+            }
+            set
+            {
+                int counter = 1;
+                int j = 0;
+                int[] grades = new int[counter];
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        grades[j] = value[i];
+                        counter++;
+                        foreach (var item in grades)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }               
+                }
+            }
+        }
+
+    }
+}
